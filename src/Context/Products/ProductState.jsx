8726526ad8486507c36a7212fd9ed3a5c89c77e8
Maruf Khan <marufkhan2002@gmail.com>
2023-01-04 18:14:48 +0530

    Fetch-data-from-axios

--- src/Context/Products/ProductState.jsx
+++ src/Context/Products/ProductState.jsx
@@ -1,13 +1,33 @@
-import React,{useContext} from 'react'
+import React, { useContext } from 'react'
+import axios from 'axios'
 import ProductContext from './ProductContext'
+import { useState, useEffect } from 'react'
 
 const ProductState = (props) => {
-  const s1 = {
-    name: 'Maruf',
-    id: '22',
+  const [data, setData] = useState(null)
+
+  // Fetching data
+  async function fetchData() {
+    try {
+      const response = await axios.get(
+        'https://api.pujakaitem.com/api/products',
+      )
+      console.log(response)
+      return response.data
+    } catch (error) {
+      console.error(error)
+    }
   }
+  useEffect(() => {
+    async function fetchDataAndSet() {
+      const data = await fetchData()
+      setData(data)
+    }
+    fetchDataAndSet()
+  }, []) // Runs in one time
+
   return (
-    <ProductContext.Provider value={s1}>
+    <ProductContext.Provider value={'maruf'}>
       {props.children}
     </ProductContext.Provider>
   )
