637e966be71d9680ea23ba220d401509c200942e
Maruf Khan <marufkhan2002@gmail.com>
2023-03-02 15:06:25 +0530

    func-localstorage-clear-cart

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,7 +1,16 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useReducer, useEffect } from "react";
 import reducer from "../Reducers/CartReducer";
 
 const CartContext = createContext();
+
+const getLocalCartData = () => {
+  let localCartData = localStorage.getItem("thapaCart");
+  if (localCartData === []) {
+    return [];
+  } else {
+    return JSON.parse(localCartData);
+  }
+};
 
 const initialState = {
   cart: [],
@@ -17,12 +26,47 @@ const CartProvider = ({ children }) => {
     dispatch({ type: "ADD_TO_CART", payload: { id, color, amount, product } });
   };
 
+  // Increment and Decrement the product
+
+  const setDecrease = (id) => {
+    dispatch({ type: "SET_DECREMENT", payload: id });
+  };
+
+  const setIncrement = (id) => {
+    dispatch({ type: "SET_INCREMENT", payload: id });
+  };
+
+  // to remove the individual item from cart
   const removeItem = (id) => {
     dispatch({ type: "REMOVE_ITEM", payload: id });
   };
 
+  // to clear the cart
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  // to add the data in localStorage
+  // get vs set
+
+  useEffect(() => {
+    dispatch({ type: "CART_TOTAL_ITEM" });
+    dispatch({ type: "CART_TOTAL_PRICE" });
+    localStorage.setItem("thapaCart", JSON.stringify(state.cart));
+  }, [state.cart]);
+
+
   return (
-    <CartContext.Provider value={{ ...state, addToCart, removeItem }}>
+    <CartContext.Provider
+      value={{
+        ...state,
+        addToCart,
+        removeItem,
+        clearCart,
+        setDecrease,
+        setIncrement,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,24 +1,64 @@
 import { useCartContext } from "../Context/CartContext";
 import CartItem from "../Components/CartItem";
+import { NavLink } from "react-router-dom";
+import FormatPrice from "../Helpers/FormatPrice";
 
 const Cart = () => {
-  const { cart } = useCartContext();
+  const { cart, clearCart, total_price, shipping_fee } = useCartContext();
   // console.log("🚀 ~ file: Cart.js ~ line 6 ~ Cart ~ cart", cart);
+
+  if (cart.length === 0) {
+    return <h3>No Cart in Item </h3>;
+  }
+
   return (
-    <div className="">
-      <div className="flex space-x-44">
-        <p>Item</p>                                                 
-        <p className="">Price</p>
+    <div className="container">
+      <div className="cart_heading grid grid-five-column">
+        <p>Item</p>
+        <p className="cart-hide">Price</p>
         <p>Quantity</p>
-        <p className="">Subtotal</p>
+        <p className="cart-hide">Subtotal</p>
         <p>Remove</p>
       </div>
       <hr />
-      <div className="">
+      <div className="cart-item">
         {cart.map((curElem) => {
-        console.log(curElem)
           return <CartItem key={curElem.id} {...curElem} />;
         })}
+      </div>
+      <hr />
+      <div className="cart-two-button">
+        <NavLink to="/products">
+          <button> continue Shopping </button>
+        </NavLink>
+        <button className="btn btn-clear" onClick={clearCart}>
+          clear cart
+        </button>
+      </div>
+
+      {/* order total_amount */}
+      <div className="order-total--amount">
+        <div className="order-total--subdata">
+          <div>
+            <p>subtotal:</p>
+            <p>
+              <FormatPrice price={total_price} />
+            </p>
+          </div>
+          <div>
+            <p>shipping fee:</p>
+            <p>
+              <FormatPrice price={shipping_fee} />
+            </p>
+          </div>
+          <hr />
+          <div>
+            <p>order total:</p>
+            <p>
+              <FormatPrice price={shipping_fee + total_price} />
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
