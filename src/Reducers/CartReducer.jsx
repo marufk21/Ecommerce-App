caf047cad3718fd6f261d906d8549e8e85da91a9
Maruf Khan <marufkhan2002@gmail.com>
2023-03-31 02:14:36 +0530

    bug-fix

--- src/Reducers/CartReducer.jsx
+++ src/Reducers/CartReducer.jsx
@@ -104,20 +104,20 @@ const CartReducer = (state, action) => {
     };
   }
 
-  if (action.type === "CART_TOTAL_ITEM") {
-    let updatedItemVal = 0;
-    if (state.cart) {
-      updatedItemVal = state.cart.reduce((initialVal, curElem) => {
-        let { amount } = curElem;
-        initialVal = initialVal + amount;
-        return initialVal;
-      }, 0);
-    }
-    return {
-      ...state,
-      total_item: updatedItemVal,
-    };
-  }
+  // if (action.type === "CART_TOTAL_ITEM") {
+  //   let updatedItemVal = 0;
+  //   if (state.cart) {
+  //     updatedItemVal = state.cart.reduce((initialVal, curElem) => {
+  //       let { amount } = curElem;
+  //       initialVal = initialVal + amount;
+  //       return initialVal;
+  //     }, 0);
+  //   }
+  //   return {
+  //     ...state,
+  //     total_item: updatedItemVal,
+  //   };
+  // }
 
   if (action.type === "CART_TOTAL_PRICE") {
     let total_price = state.cart.reduce((initialVal, curElem) => {
