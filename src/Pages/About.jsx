5fd01d2b37337970606a7c9a2ad32e5750a555d8
Maruf Khan <marufkhan2002@gmail.com>
2023-05-09 03:30:41 +0530

    improve-footer&about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
-import HeroSection from '../Components/HeroSection'
-
-
-const About = () => {
-  const data = {
-    name: 'AboutPage'
-  }
-  return <HeroSection mydata={data} />
-  
+function About() {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-2xl font-bold mb-4">Our Store</h1>
+      <p className="mb-4">
+        Our store is dedicated to providing the latest and greatest electronics
+        to our customers. We believe in offering high-quality products at
+        affordable prices, and we always put our customers first.
+      </p>
+      <p className="mb-4">
+        Our team of experts carefully selects each product in our store,
+        ensuring that we offer only the best to our customers. We're passionate
+        about technology and are always looking for new and innovative products
+        to add to our inventory.
+      </p>
+      <h2 className="text-2xl font-bold mb-4">Our Mission</h2>
+      <p className="mb-4">
+        Our mission is to provide our customers with a wide range of
+        high-quality electronics at affordable prices. We believe that everyone
+        should have access to the latest technology, and we're dedicated to
+        making that a reality.
+      </p>
+      <p className="mb-4">
+        We're committed to providing exceptional customer service and making
+        sure that our customers are always satisfied with their purchases.
+        Whether you have a question about a product or need help with your
+        order, our team is always here to help.
+      </p>
+      <h2 className="text-2xl font-bold mb-4">Our Promise</h2>
+      <p className="mb-4">
+        At our store, we stand behind every product we sell. We're committed to
+        providing our customers with high-quality products that meet their needs
+        and exceed their expectations. If for any reason you're not completely
+        satisfied with your purchase, we'll do everything we can to make it
+        right.
+      </p>
+      <p className="mb-4">
+        We value your trust and are committed to providing a safe and secure
+        shopping experience. Our website uses the latest technology to ensure
+        that your personal and payment information is always protected.
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
