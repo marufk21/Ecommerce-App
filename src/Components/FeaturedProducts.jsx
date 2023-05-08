28a2464a465976ce3c73bae4811d2e9a6faa8525
Maruf Khan <marufkhan2002@gmail.com>
2023-05-09 03:03:11 +0530

    improve-homepage

--- src/Components/FeaturedProducts.jsx
+++ src/Components/FeaturedProducts.jsx
@@ -1,30 +1,30 @@
-import { useProductContext } from '../Context/ProductState'
-import Product from './Product'
+import { useProductContext } from "../Context/ProductState";
+import Product from "./Product";
 
 const FeaturedProducts = () => {
-  const { isLoading, featureProducts } = useProductContext()
-  
+  const { isLoading, featureProducts } = useProductContext();
+
   if (isLoading) {
-    return <div>...Loading</div>
+    return;
   }
   return (
     <>
       <section className="bg-white dark:bg-gray-900">
         <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-24 lg:px-6">
           <div className="max-w-screen-md mx-auto mb-8 text-center lg:mb-12">
-            <h2 className="mb-4 text-5xl font-extrabold tracking-tight text-gray-900 dark:text-white">
+            <h2 className="my-2 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
               Our Products
             </h2>
           </div>
           <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
             {featureProducts.map((curElem) => {
-              return <Product key={curElem.id} {...curElem} />
+              return <Product key={curElem.id} {...curElem} />;
             })}
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default FeaturedProducts
+export default FeaturedProducts;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,25 +1,17 @@
-import React from 'react'
-import { Navbar } from 'flowbite-react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { Navbar } from "flowbite-react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <>
       <Navbar fluid={true} rounded={true} className="sticky top-0 z-10">
         <Navbar.Brand href="/">
-          {/* Logo */}
-          {/* <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt="Ecommerce Logo"
-          /> */}
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          ElectriKart
+            ElectriKart
           </span>
         </Navbar.Brand>
         <div className="flex md:order-2">
-          {/* Buttton */}
-          {/* <Button className=" text-white bg-purple-700">Get started</Button> */}
           <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
@@ -52,13 +44,13 @@ const NavBar = () => {
               to="/contact"
               className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
             >
-              Contact
+              Contact Us
             </NavLink>
           </li>
         </Navbar.Collapse>
       </Navbar>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
