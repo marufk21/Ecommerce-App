2a472de877345e8edc50fd65c6c88f59263cea3b
Maruf Khan <marufkhan2002@gmail.com>
2023-05-11 02:23:09 +0530

    optimise-app

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -16,41 +16,49 @@ const CartItem = ({ id, name, image, color, price, amount }) => {
   // };
 
   return (
-<div className="flex items-center py-4 border-b border-gray-300">
-  <div className="flex-shrink-0">
-    <img className="w-20 h-20 rounded-lg" src={image} alt={id} />
-  </div>
-  <div className="flex flex-col justify-between ml-4 flex-grow">
-    <div className="flex justify-between items-center">
-      <p className="text-lg font-semibold">{name}</p>
-      <FaTrash className="text-gray-400 hover:text-red-500 cursor-pointer" onClick={() => removeItem(id)} />
-    </div>
-    <div className="flex items-center mt-2">
-      <p className="mr-2 font-medium text-gray-500">Color:</p>
-      <div className="w-4 h-4 rounded-full" style={{ backgroundColor: color, color: color }}></div>
-    </div>
-    <div className="flex items-center mt-2">
-      <p className="font-medium text-gray-500">Price:</p>
-      <p className="ml-2 font-semibold"><FormatPrice price={price} /></p>
-    </div>
-    <div className="flex items-center mt-2">
-      <p className="font-medium text-gray-500">Quantity:</p>
-      <div className="flex items-center ml-2">
-        <CartAmountToggle
-          amount={amount}
-          setDecrease={() => setDecrease(id)}
-          setIncrease={() => setIncrement(id)}
-        />
+    <div className="flex items-center py-4 border-b border-gray-300">
+      <div className="flex-shrink-0">
+        <img className="w-20 h-20 rounded-lg" src={image} alt={id} />
+      </div>
+      <div className="flex flex-col justify-between ml-4 flex-grow">
+        <div className="flex justify-between items-center">
+          <p className="text-lg font-semibold">{name}</p>
+          <FaTrash
+            className="text-gray-400 hover:text-red-500 cursor-pointer"
+            onClick={() => removeItem(id)}
+          />
+        </div>
+        <div className="flex items-center mt-2">
+          <p className="mr-2 font-medium text-gray-500">Color:</p>
+          <div
+            className="w-4 h-4 rounded-full"
+            style={{ backgroundColor: color, color: color }}
+          ></div>
+        </div>
+        <div className="flex items-center mt-2">
+          <p className="font-medium text-gray-500">Price:</p>
+          <p className="ml-2 font-semibold">
+            <FormatPrice price={price} />
+          </p>
+        </div>
+        <div className="flex items-center mt-2">
+          <p className="font-medium text-gray-500">Quantity:</p>
+          <div className="flex items-center ml-2">
+            <CartAmountToggle
+              amount={amount}
+              setDecrease={() => setDecrease(id)}
+              setIncrease={() => setIncrement(id)}
+            />
+          </div>
+        </div>
+        <div className="flex items-center mt-2">
+          <p className="font-medium text-gray-500">Total:</p>
+          <p className="ml-2 font-semibold">
+            <FormatPrice price={price * amount} />
+          </p>
+        </div>
       </div>
     </div>
-    <div className="flex items-center mt-2">
-      <p className="font-medium text-gray-500">Total:</p>
-      <p className="ml-2 font-semibold"><FormatPrice price={price * amount} /></p>
-    </div>
-  </div>
-</div>
-
-
   );
 };
 
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useFilterContext } from '../Context/FilterContext'
 import GridView from './GridView'
-import ListView from './ListView'
 
 const ProductList = () => {
   const { filter_products, grid_view } = useFilterContext()
@@ -10,9 +9,6 @@ const ProductList = () => {
     return <GridView products={filter_products} />
   }
 
-  if (grid_view === false) {
-    return <ListView products={filter_products} />
-  }
 }
 
 export default ProductList
